c446981247816a663f0f20a2a55641b99954eaaf
steefbrown <steefbrown@gmail.com>
2015-11-20 19:49:39 -0700

    Deleting uncessary files

--- CPU/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Users/Steve Brown/Utah/Fall 2015/ECE 6710/Team Project/GIT/CPU/reg_alu_tb_isim_beh.exe" -prj "C:/Users/Steve Brown/Utah/Fall 2015/ECE 6710/Team Project/GIT/CPU/reg_alu_tb_beh.prj" "work.reg_alu_tb" "work.glbl" 
