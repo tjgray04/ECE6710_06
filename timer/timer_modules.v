5bc7c23bc2615395a6564649561440dc5d1cd874
timgrant <tim@rootdown.net>
2015-12-06 14:52:32 -0700

    Cleaned up the timer module a little bit.

--- timer/timer_modules.v
+++ timer/timer_modules.v
@@ -1,42 +1,44 @@
 `timescale 1ns / 1ps
 
-module Timer_100us(
-  input             clk,
-  input             reset,  
-  input             read,
-  output reg [15:0] count_out
-  );
+module Timer_100us
+(
+   input             clk,
+   input             reset,
+   input             read,
+   output reg [15:0] count_out
+);
 
-  reg [15:0] count = 0;
-  reg [10:0] ticks = 0;
-  reg tock;
-  
-  always @(posedge clk) begin
-    if (read)
-      count_out <= count;
-    else
-      count_out <= count_out;
-  end
-  
-  // generate a tock pulse every 100us
-  always @(posedge clk) begin
-    tock <= 0;
-    if (reset)
-      ticks <= 0;
-    else if (ticks < 1249)  // NOTE: 1250 ticks is 100us @ 12.5Mhz
-      ticks <= ticks + 1'b1;
-    else begin
-      ticks <= 0;
-      tock  <= 1;
-    end      
-  end
-  
-  // count the number of tocks, using wrap-around addition
-  always @(posedge tock) begin
-    if (reset)
-      count <= 0;
-    else
-      count <= count + 1'b1;      
-  end
+   // 1250 cycles is 100us @ 12.5MHz
+   localparam TICKCOUNT = 1249;
+
+   // increments every 100us, wraparound behavior
+   reg [15:0] count = 0;
+
+   // counts down the number of clock cycles until the next count increment
+   reg [10:0] ticks = 0;
+
+   // latch the count data to hold it stable during reads
+   always @(posedge clk) begin
+      if (read)
+         count_out <= count;
+      else
+         count_out <= count_out;
+   end
+
+   // counter block
+   always @(posedge clk) begin
+      if (reset) begin
+         ticks <= TICKCOUNT;
+         count <= 0;
+      end
+      else if (ticks == 11'd0) begin
+         ticks <= TICKCOUNT;
+         count <= count + 1'b1;
+      end
+      else begin
+         ticks <= ticks - 1'b1;
+         count <= count;
+      end
+   end
 
 endmodule
